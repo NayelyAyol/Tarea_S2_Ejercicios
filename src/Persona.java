44464af7360c79d118ffbe29417b839807d3d216
Nayely Ayol <nayely.ayol@epn.edu.ec>
2025-04-27 16:30:42 -0500

    Ejercicio 1- Persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,7 +1,9 @@
 public class Persona {
+    //definir los atributos
     public String nombre, nacionalidad;
     public int edad;
 
+    //Crear un metodo para mostrar la informacion
     public void presentarse(){
         System.out.println("\n------!Hola a todos¡------\n");
         System.out.println("Mi nombre es "+nombre+"\n"+"Soy de "+nacionalidad+"\n"+"Y tengo "+edad);
--- src/ProgramaPersona.java
+++ src/ProgramaPersona.java
@@ -1,9 +1,14 @@
 public class ProgramaPersona {
     public static void main(String[] args) {
+        //Crear una nueva instancia
         Persona persona1=new Persona();
+
+        //Dar valores a los atributos
         persona1.nombre="Liam";
         persona1.nacionalidad="Italia";
         persona1.edad=20;
+        
+        //Llamar al metodo para mostrar la informacion
         persona1.presentarse();
     }
 }
